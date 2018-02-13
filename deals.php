aaa26df6cf9fca78f956ac73a00a9c0416010c02
grayswand1r <josephhane@gmail.com>
2018-02-13 18:29:57 -0500

    Update deals.php

--- deals.php
+++ deals.php
@@ -3,44 +3,40 @@
 	<head>
 	</head>
 	
-<body>
-
-<H1>Here are the current deals listed on <a href="https://deals.kinja.com/all-the-best-deals-1822529788">Kinja...</a></H1>
-
-<table>
+	<body>
+		<H1>Here are the current deals listed on <a href="https://deals.kinja.com/all-the-best-deals-1822529788">Kinja...</a></H1>
+			<table>
 
 <?php
 
-//...include the DOM api for handling html
+// Include the DOM api for handling html
 include 'phpQuery.php'; 
 
-//...declare website to scrape
+// Declare website to scrape
 $url = 'https://deals.kinja.com/all-the-best-deals-1822529788'; 
 
-//...initiate phpquery
+// Initiate phpquery
 $doc = phpQuery::newDocumentFile($url); 
 
-//...set all instances of 'figcaption' to the object variable $deals
-$deals = $doc['figcaption']; 
-
-//...declare $deals_ar as an array
+// Set all instances of 'figcaption' to the object variable $deals
+$deals = $doc['figcaption'];
+	
+// Declare $deals_ar as an array
 $deals_ar = array(); 
 
-
-//...begin for loop  
+// Begin for loop  
 foreach ($deals as $deal) { 
 
-	//...add the text content of each $deal to the array $deals_ar
+	// Add the text content of each $deal to the array $deals_ar
 	$deals_ar[] = $deal->textContent; 
 	
-	//...print each $deal on a new line
+	// Print each $deal on a new line
 	echo '<tr><td>' . pq($deal) . '</td></tr>'; 
 	
 }
 
 ?>
 
-</table>
-
-</body>
+			</table>
+	</body>
 </html>
